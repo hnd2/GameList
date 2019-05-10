5c32b0ab87ab80b1fc18bc012a559793cfed0587
hnd <>
2019-05-10 12:59:34 +0900

    aa!

---
+++ list.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    name = "First Game",
+    git = "https://github.com/hnd2/FirstGame",
+    thumb = "thumbs/firstgame.png",
+  },
+  {
+    name = "Second Game",
+    git = "https://github.com/hnd2/SecondGame",
+    thumb = "thumbs/secondgame.png",
+  },
+  {
+    name = "Third Game",
+    git = "https://github.com/hnd2/ThirdGame",
+    thumb = nil,
+  },
+}
