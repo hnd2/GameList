ce487a0d5fbbe7a2515a19f1938d755d40cb1c38
hnd2 <45590846+hnd2@users.noreply.github.com>
2019-05-16 17:24:49 +0900

    Update list.lua
    aa

--- list.lua
+++ list.lua
@@ -2,16 +2,16 @@ return {
   {
     name = "First Game",
     git = "https://github.com/hnd2/FirstGame",
-    thumb = "thumbs/firstgame.png",
+    thumbPath = "thumbs/firstgame.png",
   },
   {
     name = "Second Game",
     git = "https://github.com/hnd2/SecondGame",
-    thumb = "thumbs/secondgame.png",
+    thumbPath = "thumbs/secondgame.png",
   },
   {
     name = "Third Game",
     git = "https://github.com/hnd2/ThirdGame",
-    thumb = nil,
+    thumbPath = nil,
   },
 }
